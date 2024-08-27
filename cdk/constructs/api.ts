706029f15cd799a8ee9c4598e91c6bb6460b7ad9
Bent Krause <bentkr@amazon.de>
2024-08-27 16:19:29 +0200

    Adding Request validation for path and body at the API Gateway level

--- cdk/constructs/api.ts
+++ cdk/constructs/api.ts
@@ -41,22 +41,75 @@ export class Api extends Construct {
         const updateResource = projectWithId.addResource('update');
         const deleteResource = projectWithId.addResource('delete');
         
+        const createProjectRequestModel = new apigateway.Model(this, 'createProjectRequestModel', {
+            restApi: this.apigateway,
+            contentType: 'application/json',
+            description: 'Validate if all required inputs are provided by the requestor',
+            modelName: 'createProjectRequestModel',
+            schema : {
+                type: apigateway.JsonSchemaType.OBJECT,
+                required: ["PollSchedule", "PollUrl", "SecretArn", "TenantAccountRoleArn", "KinesisStreamArn"],
+                properties: {
+                    PollSchedule: { type: apigateway.JsonSchemaType.STRING },
+                    PollUrl: { type: apigateway.JsonSchemaType.STRING },
+                    SecretArn: { type: apigateway.JsonSchemaType.STRING },
+                    TenantAccountRoleArn: { type: apigateway.JsonSchemaType.STRING },
+                    KinesisStreamArn: { type: apigateway.JsonSchemaType.STRING }
+                }
+            }
+        })
+
         createResource.addMethod('POST', props.createResourceIntegration, {
         methodResponses: [
             { statusCode: '200' },
             { statusCode: '400' },
             { statusCode: '500' }
-        ]
+        ],
+        requestValidator: new apigateway.RequestValidator(this, 'createResourceRequestValidator', {
+            restApi: this.apigateway,
+            validateRequestParameters: false,
+            validateRequestBody: true,
+        }),
+        requestModels: {
+            "application/json": createProjectRequestModel,
+        }
         });
         deleteResource.addMethod('DELETE', props.deleteResourceIntegration, {
         methodResponses: [
             { statusCode: '200' },
             { statusCode: '400' },
             { statusCode: '500' }
-        ]
+        ],
+        requestParameters: {
+            'method.request.path.id': true
+        },
+        requestValidator: new apigateway.RequestValidator(this, 'deleteResourceRequestValidator', {
+            restApi: this.apigateway,
+            validateRequestParameters: true,
+            validateRequestBody: false
+        }),
         });
-        listResources.addMethod('GET', new apigateway.LambdaIntegration(props.listResourcesFunction));
-        updateResource.addMethod('PUT', new apigateway.LambdaIntegration(props.updateResourceFunction));
+        listResources.addMethod('GET', new apigateway.LambdaIntegration(props.listResourcesFunction), {
+        methodResponses: [
+            { statusCode: '200' },
+            { statusCode: '400' },
+            { statusCode: '500' }
+        ],
+        });
+        updateResource.addMethod('PUT', new apigateway.LambdaIntegration(props.updateResourceFunction), {
+        methodResponses: [
+            { statusCode: '200' },
+            { statusCode: '400' },
+            { statusCode: '500' }
+        ],
+        requestParameters: {
+            'method.request.path.id': true
+        },
+        requestValidator: new apigateway.RequestValidator(this, 'updateResourceRequestValidator', {
+            restApi: this.apigateway,
+            validateRequestParameters: true,
+            validateRequestBody: false
+        }),
+        });
     }
 }
-
